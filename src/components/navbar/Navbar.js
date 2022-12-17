da99a181d07e322a50dee363f9f4ec581dd91688
ashkan20171 <ashkan.mataee@yahoo.com>
2022-12-17 10:45:43 +0330

    add project

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { AiOutlineBars } from "react-icons/ai";
+import { RiCloseLine } from "react-icons/ri";
+import { SiAnaconda } from "react-icons/si";
+import './Navbar.css';
+
+const Navbar = () => {
+  const [showIcon , setShowIcon] = useState(true);
+
+  const toggleMenu = () => {
+    setShowIcon(!showIcon);
+  }
+
+  return (
+    <nav className="navbar container">
+      <div className="menu">
+        <ul className="nav-links" id={showIcon ? "show-links-mobile" : "show-links-mobile-hide" }>
+          <li><a href="" className="btn btn-dark">آموزش بیشتر</a></li>
+          <li><a href="">خانه</a></li>
+          <li><a href="">امکانات</a></li>
+          <li><a href="">دانلود</a></li>
+          <li><a href="">مشترک شوید</a></li>
+        </ul>
+      </div>
+      <div className="logo">
+        <SiAnaconda color="#fff" size={30} />
+        <p className='logo-text'>
+          ashkan
+          <span>m</span>
+        </p>
+      </div>
+
+      <div className="menu-icons" onClick={toggleMenu}>
+        {
+          showIcon ? 
+          <AiOutlineBars size={35} color={"#fff"} cursor={"pointer"} /> :
+           <RiCloseLine size={35} color={"#fff"} cursor={"pointer"}/>
+        }
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
