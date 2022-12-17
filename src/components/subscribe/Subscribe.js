0202d6584870546404ca641886388bd401f9039f
ashkan20171 <ashkan.mataee@yahoo.com>
2022-12-17 17:35:41 +0330

    add subscribe

--- src/components/subscribe/Subscribe.js
+++ src/components/subscribe/Subscribe.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Subscribe = () => {
-  return <div>مشترک</div>;
+  return (
+    <section id="subscribe">
+      <div className="container subscribe">
+        <h2>حالا مشترک شوید!</h2>
+        <form>
+          <button>مشترک شوید</button>
+          <input type="text" placeholder='ایمیل خود را وارد کنید ...' />
+        </form>
+      </div>
+    </section>
+  )
 };
 
 export default Subscribe;
