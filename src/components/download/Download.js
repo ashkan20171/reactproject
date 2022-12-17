6b1f866e34424ee46e085ccf8aaa2a732d654791
ashkan20171 <ashkan.mataee@yahoo.com>
2022-12-17 17:01:47 +0330

    add download

--- src/components/download/Download.js
+++ src/components/download/Download.js
@@ -1,9 +1,36 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import { FaLinux, FaWindows} from "react-icons/fa";
+import { GrAndroid } from "react-icons/gr";
+import './Download.css';
+
 
 function Download() {
-  return <div>
-       دانلود
-  </div>;
+  return (
+    <section id="download">
+     <div className="container download">
+       <div className="download-title">
+        <h2>دانلود اپلیکیشن ما</h2>
+        <p>برنامه های ما برای دانلود در همه ی فروشگاه ها موجود است.</p>
+
+       </div>
+       <div className="download-apps">
+         <div className="download-app">
+            <FaLinux /> 
+            <p>لینوکس</p>
+         </div>
+         <div className="download-app">
+            <FaWindows /> 
+            <p>ویندوز</p>
+         </div>
+         <div className="download-app">
+            <GrAndroid /> 
+            <p>اندروید</p>
+         </div>
+       </div>
+     </div>
+    </section>
+  )
 }
 
 export default Download;
