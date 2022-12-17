8837e99a5c2456efef25d5edb61d6b38a6d147df
ashkan20171 <ashkan.mataee@yahoo.com>
2022-12-17 17:56:07 +0330

    edit subscribe

--- src/components/subscribe/Subscribe.js
+++ src/components/subscribe/Subscribe.js
@@ -1,4 +1,9 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import { TiSocialGooglePlus } from "react-icons/ti";
+import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const Subscribe = () => {
   return (
@@ -9,6 +14,21 @@ const Subscribe = () => {
           <button>مشترک شوید</button>
           <input type="text" placeholder='ایمیل خود را وارد کنید ...' />
         </form>
+
+        <div className="socials">
+           <div className="social-icon">
+              <FaInstagram />
+           </div>
+           <div className="social-icon">
+              <FaTwitter />
+           </div>
+           <div className="social-icon">
+              <FaFacebookF />
+           </div>
+           <div className="social-icon">            
+              <TiSocialGooglePlus />              
+           </div>
+        </div>
       </div>
     </section>
   )
