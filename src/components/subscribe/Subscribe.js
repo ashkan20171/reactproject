fd2b375a65959007f4fdfc2f8dc366a138288af7
ashkan20171 <ashkan.mataee@yahoo.com>
2022-12-18 10:10:35 +0330

    complete subscribe

--- src/components/subscribe/Subscribe.js
+++ src/components/subscribe/Subscribe.js
@@ -1,9 +1,7 @@
-/* eslint-disable jsx-a11y/anchor-has-content */
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import { TiSocialGooglePlus } from "react-icons/ti";
 import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
+import './Subscribe.css'
 
 const Subscribe = () => {
   return (
@@ -11,27 +9,27 @@ const Subscribe = () => {
       <div className="container subscribe">
         <h2>حالا مشترک شوید!</h2>
         <form>
-          <button>مشترک شوید</button>
+          <button>مشترک شدن</button>
           <input type="text" placeholder='ایمیل خود را وارد کنید ...' />
         </form>
 
         <div className="socials">
-           <div className="social-icon">
-              <FaInstagram />
-           </div>
-           <div className="social-icon">
-              <FaTwitter />
-           </div>
-           <div className="social-icon">
-              <FaFacebookF />
-           </div>
-           <div className="social-icon">            
-              <TiSocialGooglePlus />              
-           </div>
+          <div className="social-icon">
+            <FaInstagram />
+          </div>
+          <div className="social-icon">
+            <FaTwitter />
+          </div>
+          <div className="social-icon">
+            <FaFacebookF />
+          </div>
+          <div className="social-icon">
+            <TiSocialGooglePlus />
+          </div>
         </div>
       </div>
     </section>
-  )
+    );
 };
 
 export default Subscribe;
